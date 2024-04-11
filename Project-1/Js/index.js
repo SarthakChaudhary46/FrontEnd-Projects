dfea03ef5ed7ecc093b11ff130fbc379929e6653
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-04-11 11:49:07 +0530

    Update index.js

--- Project-1/Js/index.js
+++ Project-1/Js/index.js
@@ -1,4 +1,4 @@
-const endDate = "15 March 2024 10:00 PM"
+const endDate = "15 April 2024 10:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
